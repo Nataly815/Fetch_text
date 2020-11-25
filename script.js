6f0f1bf67e7a86b8916b2aedd8e2758bae0ea0aa
Natasha <nataly815@gmail.com>
2020-11-25 18:08:08 +0300

    responsive answer from fetch request added

--- script.js
+++ script.js
@@ -5,18 +5,29 @@ let username = document.querySelector('.username');
 let userAvatar = document.querySelector('.userAvatar');
 let userBio = document.querySelector('.bio');
 let userUrl = document.querySelector('.url');
+let url = window.location.toString();
 
-let name = 'Nataly815';
+function getName(url){
+    let nameUrl = url.split('=')[1];
+    if (nameUrl == undefined) {
+        nameUrl = 'Nataly815';
+    }
+    return nameUrl;
+}
+
+let name = getName(url);
 
 fetch(`https://api.github.com/users/${name}`)
     .then(res => res.json())
     .then(json => {
+        if (json.login !== undefined){
         nickname.innerHTML = json.login;
         username.innerHTML = json.name;
         userAvatar.src = json.avatar_url;
         userBio.innerHTML = json.bio;
         userUrl.innerHTML = json.html_url;
+        } else {
+        document.body.innerHTML = 'Информация о пользователе не доступна';
+        }
     })
-    .catch(err => h1.innerHTML = 'Информация о пользователе не доступна');
-
-    
+    .catch(err => document.body.innerHTML = 'Информация о пользователе не доступна');
